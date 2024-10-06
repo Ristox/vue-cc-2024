d3b58efdfd91fb3162ad5501ca59c9d1afc0cb94
Risto Uibo <risto@rsx.ee>
2024-10-06 15:19:50 +0300

    App.vue | create Navbar, Title and Home Cards components

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,11 @@
-<script>
-export default {
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'inactive',
-      tasks: ['Buy milk', 'Go for a walk', 'Clean room', 'Go to the gym'],
-      link: 'https://google.com',
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === 'active') {
-        this.status = 'inactive';
-      } else if (this.status === 'inactive') {
-        this.status = 'pending';
-      } else {
-        this.status = 'active';
-      }
-    },
-  }
-};
+
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Title from "@/components/Title.vue";
+import HomeCards from "@/components/HomeCards.vue";
 </script>
-
 <template>
-  <h1>{{ name }}</h1>
-
-  <div>
-    <button @click="toggleStatus">Change status</button>
-  </div>
-
-  <div v-if="status === 'active'">
-    <p>User is active</p>
-    <h3>Tasks:</h3>
-    <ul>
-      <li v-for="task in tasks" :key="task">{{ task }}</li>
-    </ul>
-  </div>
-
-  <p v-else-if="status === 'pending'">
-    User is pending
-  </p>
-  <p v-else>
-    User is not active
-  </p>
-
-  <div>
-    <a :href="link">Click me</a>
-  </div>
+  <Navbar />
+  <Title title="Test title" subtitle="This is a test subtitle here"/>
+  <HomeCards />
 </template>
-
-<style scoped>
-
-</style>
